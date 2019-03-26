5eeb1b909ae4a0e38434097793dc14a38f79fc6e
root <root@ip-172-31-34-221.us-east-2.compute.internal>
2019-03-26 09:31:23 +0000

    implement feedback add lines between functions create main function

--- app.sh
+++ app.sh
@@ -1,20 +1,25 @@
+#!/usr/bin bash
 . .env
 function install_node {
   curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
   sudo bash nodesource_setup.sh 
   sudo apt-get install -y nodejs
 }
+
 function clone_repository {
   git clone ${github_repo}
 }
+
 function install_application_dependencies {
   cd ${repo_name}
   sudo npm install node-pre-gyp -ES --unsafe-perm=true
   sudo npm i -ES --unsafe-perm=true
 }
+
 function build_webpack {
   sudo npm run build
 }
+
 configuration_server="
   server {
    server_name storeman.ga www.storeman.ga;
@@ -23,6 +28,7 @@ configuration_server="
     }
   }
 "
+
 function nginx_configuration {
   sudo apt-get install nginx -y
   sudo chmod 777 /etc/nginx
@@ -32,6 +38,7 @@ function nginx_configuration {
   sudo ln -s /etc/nginx/sites-available/${repo_name} /etc/nginx/sites-enabled/${repo_name}
   sudo service nginx start
 }
+
 function ssl_configuration {
   sudo apt-get update
   sudo apt-get install software-properties-common
@@ -40,6 +47,7 @@ function ssl_configuration {
   sudo apt-get install certbot python-certbot-nginx -y
   sudo certbot --nginx --expand -d storeman.ga -d www.storeman.ga -m ${email} --agree-tos --non-interactive
 }
+
 start_script='
   {
     "apps": [
@@ -51,15 +59,20 @@ start_script='
     ]
   }
 '
+
 function process_manager {
   sudo npm install pm2 -g
   sudo echo ${start_script} > ./start_script.config.json
   pm2 start start_script.config.json
 }
-install_node
-clone_repository
-install_application_dependencies
-build_webpack
-nginx_configuration
-ssl_configuration
-process_manager
+
+function main {
+ install_node
+ clone_repository
+ install_application_dependencies
+ build_webpack
+ nginx_configuration
+ ssl_configuration
+ process_manager
+}
+main
